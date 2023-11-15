d6e4009d1629c82b3dc98b45f0688a1a9433e6a4
Meiirbek Baimuratov <meirbekbaimuratov@gmail.com>
2023-11-15 16:54:58 +0600

    feat: use data from provider

--- lib/ui/screens/profiles/profiles_page.dart
+++ lib/ui/screens/profiles/profiles_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kinder/cubit/profiles/profiles_cubit.dart';
 import 'package:kinder/ui/screens/profiles/widgets/follow_button.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -12,6 +14,7 @@ class _ProfilePageState extends State<ProfilePage> {
   final PageController _pageController = PageController();
   double _bottomSheetTopPercentage = 0.7;
   double _sheetProgress = 0.0;
+  int _currentPage = 0;
 
   @override
   void initState() {
@@ -21,6 +24,8 @@ class _ProfilePageState extends State<ProfilePage> {
         setState(() {});
       }
     });
+
+    BlocProvider.of<ProfilesCubit>(context).fetchProfiles();
   }
 
   @override
@@ -34,170 +39,197 @@ class _ProfilePageState extends State<ProfilePage> {
     double screenHeight = MediaQuery.of(context).size.height;
     double photoHeight = screenHeight * _bottomSheetTopPercentage + 50;
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 100),
-            curve: Curves.easeInOut,
-            height: photoHeight,
-            child: PageView.builder(
-              physics: const PageScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (BuildContext context, int index) {
-                return Image.network(
-                  'https://picsum.photos/500/800',
-                  fit: BoxFit.fitWidth,
-                );
-              },
-            ),
-          ),
-          NotificationListener<DraggableScrollableNotification>(
-            onNotification: (notification) {
-              setState(() {
-                _bottomSheetTopPercentage = 1 - notification.extent;
-                photoHeight = screenHeight * _bottomSheetTopPercentage + 50;
-                _sheetProgress = (notification.extent - 0.3) / 0.3;
-              });
-              return true;
-            },
-            child: DraggableScrollableSheet(
-              initialChildSize: 0.3,
-              minChildSize: 0.3,
-              maxChildSize: 0.6,
-              builder: (BuildContext context, ScrollController scrollController) {
-                return Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      decoration: const BoxDecoration(
-                        color: Colors.transparent,
-                      ),
-                      child: _buildSocialStats(),
-                    ),
-                    Expanded(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(24),
-                            topRight: Radius.circular(24),
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 10,
-                              color: Colors.black26,
+    return BlocBuilder<ProfilesCubit, ProfilesState>(
+      builder: (context, state) {
+        if (state is ProfilesLoaded) {
+          final profiles = state.profiles;
+          return Scaffold(
+            body: Stack(
+              children: [
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 100),
+                  curve: Curves.easeInOut,
+                  height: photoHeight,
+                  child: PageView.builder(
+                    physics: const PageScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: profiles.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Image.network(
+                        profiles[index].photoUrls.first,
+                        fit: BoxFit.fitWidth,
+                      );
+                    },
+                    onPageChanged: (index) {
+                      setState(() {
+                        _currentPage = index;
+                      });
+                    },
+                  ),
+                ),
+                NotificationListener<DraggableScrollableNotification>(
+                  onNotification: (notification) {
+                    setState(() {
+                      _bottomSheetTopPercentage = 1 - notification.extent;
+                      photoHeight = screenHeight * _bottomSheetTopPercentage + 50;
+                      _sheetProgress = (notification.extent - 0.3) / 0.3;
+                    });
+                    return true;
+                  },
+                  child: DraggableScrollableSheet(
+                    initialChildSize: 0.3,
+                    minChildSize: 0.3,
+                    maxChildSize: 0.6,
+                    builder: (BuildContext context, ScrollController scrollController) {
+                      return Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            decoration: const BoxDecoration(
+                              color: Colors.transparent,
                             ),
-                          ],
-                        ),
-                        child: ListView(
-                          controller: scrollController,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(20),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
+                            child: _buildSocialStats(
+                              profiles[_currentPage].followers,
+                              profiles[_currentPage].posts,
+                              profiles[_currentPage].following,
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(24),
+                                  topRight: Radius.circular(24),
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    blurRadius: 10,
+                                    color: Colors.black26,
+                                  ),
+                                ],
+                              ),
+                              child: ListView(
+                                controller: scrollController,
                                 children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Column(
-                                        mainAxisSize: MainAxisSize.min,
+                                  Padding(
+                                    padding: const EdgeInsets.all(20),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  profiles[_currentPage].name,
+                                                  style: const TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 18,
+                                                  ),
+                                                ),
+                                                const SizedBox(height: 4),
+                                                Text(
+                                                  profiles[_currentPage].location,
+                                                  style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Colors.grey[600],
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            AnimatedFollowButton(
+                                              following: profiles[_currentPage].isFollowing,
+                                              onPressed: () {
+                                                setState(() {
+                                                  profiles[_currentPage].isFollowing = !profiles[_currentPage].isFollowing;
+                                                });
+                                              },
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Opacity(
+                                    opacity: _sheetProgress.clamp(0.0, 1.0),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                                      child: Text(
+                                        profiles[_currentPage].bio ?? '',
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 20),
+                                  Opacity(
+                                    opacity: _sheetProgress.clamp(0.0, 1.0),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                                      child: Column(
                                         crossAxisAlignment: CrossAxisAlignment.start,
                                         children: [
                                           const Text(
-                                            'Christine Wallace',
+                                            'Photos',
                                             style: TextStyle(
                                               fontWeight: FontWeight.bold,
                                               fontSize: 18,
                                             ),
                                           ),
-                                          const SizedBox(height: 4),
-                                          Text(
-                                            'France, Perpignan',
-                                            style: TextStyle(
-                                              fontSize: 16,
-                                              color: Colors.grey[600],
+                                          const SizedBox(height: 10),
+                                          SizedBox(
+                                            height: 100,
+                                            child: ListView.builder(
+                                              scrollDirection: Axis.horizontal,
+                                              itemCount: profiles[_currentPage].photoUrls.length,
+                                              itemBuilder: (BuildContext context, int index) {
+                                                return Padding(
+                                                  padding: const EdgeInsets.only(right: 10),
+                                                  child: Image.network(
+                                                    profiles[_currentPage].photoUrls[index],
+                                                    fit: BoxFit.fitWidth,
+                                                  ),
+                                                );
+                                              },
                                             ),
                                           ),
                                         ],
                                       ),
-                                      const AnimatedFollowButton(),
-                                    ],
+                                    ),
                                   ),
                                 ],
                               ),
                             ),
-                            Opacity(
-                              opacity: _sheetProgress.clamp(0.0, 1.0),
-                              child: const Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            Opacity(
-                              opacity: _sheetProgress.clamp(0.0, 1.0),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 20),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      'Photos',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 10),
-                                    SizedBox(
-                                      height: 100,
-                                      child: ListView.builder(
-                                        scrollDirection: Axis.horizontal,
-                                        itemCount: 5,
-                                        itemBuilder: (BuildContext context, int index) {
-                                          return Padding(
-                                            padding: const EdgeInsets.only(right: 10),
-                                            child: Image.network(
-                                              'https://picsum.photos/100?image=${index + 100}',
-                                              fit: BoxFit.cover,
-                                            ),
-                                          );
-                                        },
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                );
-              },
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
+          );
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 
-  Widget _buildSocialStats() {
+  Widget _buildSocialStats(int followers, int posts, int following) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          _buildSocialStat('869', 'followers'),
-          _buildSocialStat('135', 'posts'),
-          _buildSocialStat('485', 'following'),
+          _buildSocialStat(followers.toString(), 'Followers'),
+          _buildSocialStat(posts.toString(), 'Posts'),
+          _buildSocialStat(following.toString(), 'Following'),
         ],
       ),
     );
--- lib/ui/screens/profiles/widgets/follow_button.dart
+++ lib/ui/screens/profiles/widgets/follow_button.dart
@@ -1,37 +1,38 @@
 import 'package:flutter/material.dart';
 
 class AnimatedFollowButton extends StatefulWidget {
-  const AnimatedFollowButton({super.key});
+  const AnimatedFollowButton({
+    super.key,
+    this.following = false,
+    this.onPressed,
+  });
+
+  final bool following;
+  final Function()? onPressed;
 
   @override
   State createState() => _AnimatedFollowButtonState();
 }
 
 class _AnimatedFollowButtonState extends State<AnimatedFollowButton> {
-  bool isFollowing = false;
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        setState(() {
-          isFollowing = !isFollowing;
-        });
-      },
+      onTap: widget.onPressed,
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 300),
-        width: isFollowing ? 40 : 120,
+        width: widget.following ? 40 : 120,
         height: 40,
         decoration: BoxDecoration(
-          color: isFollowing ? Colors.red : Colors.white,
+          color: widget.following ? Colors.red : Colors.white,
           borderRadius: BorderRadius.circular(20),
           border: Border.all(
-            color: isFollowing ? Colors.red : Colors.red,
+            color: widget.following ? Colors.red : Colors.red,
           ),
         ),
         child: AnimatedSwitcher(
           duration: const Duration(milliseconds: 300),
-          child: isFollowing
+          child: widget.following
               ? Icon(Icons.person, color: Colors.white, key: UniqueKey())
               : Text(
                   'FOLLOW',

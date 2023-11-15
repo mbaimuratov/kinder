96fa973f21fe2f10f3ac728659580a5fd6758a11
Meiirbek Baimuratov <meirbekbaimuratov@gmail.com>
2023-11-15 21:10:02 +0600

    feat: add durations as constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+// button animation duration
+const Duration kButtonAnimationDuration = Duration(milliseconds: 300);
+
+// text animation duration
+const Duration kTextAnimationDuration = Duration(milliseconds: 300);
+
+// profile photo animation duration
+const Duration kProfilePhotoAnimationDuration = Duration(milliseconds: 50);
